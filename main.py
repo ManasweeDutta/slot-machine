e1c7e8c4bdaa87d0eb7894df0c5d57512177b051
Manaswee <manasweedutta@gmail.com>
2026-09-15 12:11:45 +0000

    -add user inputs

---
+++ main.py
@@ -0,0 +1,72 @@
+## GLOBAL CONSTANTS ##
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 10
+
+
+## USER INPUTS ##
+
+def deposit():
+    while True:
+        amount =input("How much would you like to deposit ? ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Please enter an amount greater than 0.")
+        else:
+            print("Invalid input. Please enter a numeric value.")
+            
+    return amount
+
+def get_no_of_lines():
+    while True:
+            lines =input("How many lines would you like to bet on? (1-" + str(MAX_LINES) + ")? ")
+            if lines.isdigit():
+                lines = int(lines)
+                if 1 <= lines <= MAX_LINES:
+                    break
+                else:
+                    print("Please enter a valid number of lines.")
+            else:
+                print("Invalid input. Please enter a numeric value.")
+                
+    return lines
+
+
+def get_bet():
+    while True:
+            amount =input("What would you like to bet on each line? ")
+            if amount.isdigit():
+                amount = int(amount)
+                if MIN_BET <= amount <= MAX_BET:
+                    break
+                else:
+                    print(f"Please enter an amount between {MIN_BET} and {MAX_BET}.")
+            else:
+                print("Invalid input. Please enter a numeric value.")
+                
+    return amount
+
+    
+    
+    
+def main():  
+    balance = deposit()
+    lines = get_no_of_lines()
+    
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        
+        if total_bet > balance:
+            print(f"You do not have enough balance to make that bet. Your current balance is ${balance}.")
+        else:
+            break
+        
+   
+    print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}.")
+    
+main()
+    
